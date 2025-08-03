4da32855f2560174de67d6bb6381b9c80dec096e
Cole Determan <coledeterman@gmail.com>
2025-08-03 14:22:02 -0400

    chore: update version to 1.0.17 in all relevant files

--- update-checker.js
+++ update-checker.js
@@ -1,6 +1,6 @@
 class UpdateChecker {
     constructor() {
-        this.latestVersion = '1.0.16'; // Increment this for new updates
+        this.latestVersion = '1.0.17'; // Increment this for new updates
         this.currentVersion = this.getCurrentVersion();
         this.updateCheckInterval = null;
         this.isUpdating = false;
