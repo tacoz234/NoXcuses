5db96f221d87b2085861a2cd1dcaa8a01f83014b
Cole Determan <coledeterman@gmail.com>
2025-08-03 14:34:34 -0400

    chore: bump version to 1.0.23 and update cache references

--- update-checker.js
+++ update-checker.js
@@ -1,6 +1,6 @@
 class UpdateChecker {
     constructor() {
-        this.latestVersion = '1.0.22'; // Increment this for new updates
+        this.latestVersion = '1.0.23'; // Increment this for new updates
         this.currentVersion = this.getCurrentVersion();
         this.updateCheckInterval = null;
         this.isUpdating = false;
